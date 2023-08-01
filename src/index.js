67eec44019da3a2156786c2fc8623097dbcd420e
王勇 <wy19945372694@163.com>
2023-08-01 18:36:42 +0800

    202308011736update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isValidPhone = void 0;
 function isValidPhone(phone) {
-    const reg = /^[3-9]\d${9]/;
+    const reg = /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1589]))\d{8}$/;
     return reg.test(phone);
 }
 exports.isValidPhone = isValidPhone;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /**
-* 手机号校验
-* @param 手机号 - 字符串
-*/
- 
+ * 手机号校验
+ * @param 手机号 - 字符串
+ */
+
 export function isValidPhone(phone: string): boolean {
-    const reg = /^[3-9]\d${9]/
+    const reg = /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1589]))\d{8}$/
     return reg.test(phone)
 }
